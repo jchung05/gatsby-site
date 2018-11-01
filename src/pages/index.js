b6d1e30661c736248f0088f610d6deac4253dd2d
Joey Chung <joeychung960@gmail.com>
2018-10-31 22:23:22 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>F Scott Fitzgerald wrote this</h1>
+    <p>I haven't read the book yet</p>
+    <p>But I heard it's part of the Intelliquest 100 Greatest Books series.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
